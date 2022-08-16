d129ee0c58b7335e7797cf7943c01f098060ddb3
VirajikaNirmali <90496385+VirajikaNirmali@users.noreply.github.com>
2022-08-16 15:47:59 +0530

    I create my blog app

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" href="Plugin/bootstrap.min.css">
+    <script src ="plugin/jquery.min.js"></script>
+    <script src ="plugin/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tiro+Gurmukhi&display=swap" rel="stylesheet">
+    <title>web application</title>
+</head>
+<body>
+    <?php  include_once("inc/nav.php")  ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron mt-4">
+                    <h4 id="jumbo-heder">Welcome to the Library</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
